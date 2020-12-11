56b2d6f64fe1e38f07d79f0eeeeaed40eb8ee9a1
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-11 09:48:52 +0000

    Remove the worst of the repeated code.

--- 11/11.py
+++ 11/11.py
@@ -2,6 +2,8 @@
 
 import sys
 
+part = int(sys.argv[2])
+
 with open(sys.argv[1]) as f:
     unpadded = list(map(lambda l: ['.'] + list(l.strip()) + ['.'], f.readlines()))
 
@@ -12,7 +14,22 @@ state = [padding] + unpadded + [padding]
 def dump_state(state):
     print('\n'.join(map(lambda l: ''.join(l), state)))
 
-def evolve_cell(state, y, x):
+def evaluate_neighbour2(state, y, x, dy, dx):
+    y2 = y + dy
+    x2 = x + dx
+    while y2 >= 0 and y2 < len(state) and x2 > 0 and x2 < len(state[0]):
+        if state[y2][x2] == '#':
+            return True
+
+        if state[y2][x2] == 'L':
+            return False
+
+        y2 += dy
+        x2 += dx
+
+    return False
+
+def evolve_cell(state, y, x, evaluate_fn, threshold):
     current = state[y][x]
     if current == '.':
         return '.'
@@ -22,20 +39,17 @@ def evolve_cell(state, y, x):
         for dx in range(-1, 2):
             if dy == 0 and dx == 0:
                 continue
-            if state[y+dy][x+dx] == '#':
-                occupied += 1
+            occupied += evaluate_fn(state, y, x, dy, dx)
 
-    if current == 'L':
-        return '#' if occupied == 0 else 'L'
+    next_occupied = (occupied == 0) if current == 'L' else (occupied < threshold)
+    return '#' if next_occupied else 'L'
 
-    return 'L' if occupied >= 4 else '#'
-
-def iterate(state):
+def iterate(state, evaluate_fn, threshold):
     next_state = []
     for y in range(len(state)):
         next_row = []
         for x in range(len(state[y])):
-            next_row.append(evolve_cell(state, y, x))
+            next_row.append(evolve_cell(state, y, x, evaluate_fn, threshold))
         next_state.append(next_row)
 
     return next_state
@@ -43,61 +57,13 @@ def iterate(state):
 def count_state(state):
     return sum(map(lambda r: sum(map(lambda c: c == '#', r)), state))
 
-prev_count = -1
-
-while False:
-    state = iterate(state)
-    count = count_state(state)
-    if count == prev_count:
-        break
-    prev_count = count
-
-#print(count)
-
-def evolve_cell2(state, y, x):
-    current = state[y][x]
-    if current == '.':
-        return '.'
-
-    occupied = 0
-    for dy in range(-1, 2):
-        for dx in range(-1, 2):
-            if dy == 0 and dx == 0:
-                continue
-            y2 = y + dy
-            x2 = x + dx
-            while y2 >= 0 and y2 < len(state) and x2 > 0 and x2 < len(state[0]):
-                if state[y2][x2] == '#':
-                    occupied += 1
-                    break
-
-                if state[y2][x2] == 'L':
-                    break
-
-                y2 += dy
-                x2 += dx
-
-    if current == 'L':
-        return '#' if occupied == 0 else 'L'
-
-    return 'L' if occupied >= 5 else '#'
-
-def iterate2(state):
-    next_state = []
-    for y in range(len(state)):
-        next_row = []
-        for x in range(len(state[y])):
-            next_row.append(evolve_cell2(state, y, x))
-        next_state.append(next_row)
-
-    return next_state
+evaluate_fn, threshold = (lambda s, y, x, dy, dx: s[y+dy][x+dx] == '#', 4) if part == 1 else (evaluate_neighbour2, 5)
 
 prev_count = -1
 while True:
-    state = iterate2(state)
+    state = iterate(state, evaluate_fn, threshold)
     count = count_state(state)
     if count == prev_count:
         break
     prev_count = count
-
 print(count)
