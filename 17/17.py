75f39abafd20a90aa4a7f522e6e00c9c70fcbf66
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-17 06:02:50 +0000

    Generic version, if slower.

--- 17/17.py
+++ 17/17.py
@@ -4,38 +4,39 @@ import collections
 import itertools
 import sys
 
+import numpy as np
+
+dimensions = int(sys.argv[2])
+generations = int(sys.argv[3])
+
 active = set()
 with open(sys.argv[1]) as f:
-    y = 0
-    for l in f.readlines():
-        x = 0
-        for c in l.strip():
+    for y, l in enumerate(f.readlines()):
+        for x, c in enumerate(l.strip()):
             if c == '#':
-                active.add((x, y, 0))
-            x = x + 1
-        y = y + 1
+                loc = [0] * dimensions
+                loc[0] = x
+                loc[1] = y
+                active.add(tuple(loc))
 
 STEPS = [-1, 0, 1]
-
-DIRECTIONS = list(filter(lambda s: s != (0, 0, 0), itertools.product(STEPS, STEPS, STEPS)))
+zero_step = tuple([0] * dimensions)
+DIRECTIONS = list(map(np.array, filter(lambda s: s != zero_step, itertools.product(*([STEPS] * dimensions)))))
 
 def is_active(old, count):
-    if old:
-        return count == 2 or count == 3
-    else:
-        return count == 3
+    return count == 3 or (old and count == 2)
 
 def iterate(state):
     counts = collections.defaultdict(int)
-    for cell in state:
+    for cell_tuple in state:
+        cell = np.array(cell_tuple)
         for d in DIRECTIONS:
-            new_x, new_y, new_z = cell[0] + d[0], cell[1] + d[1], cell[2] + d[2]
-            counts[(new_x, new_y, new_z)] += 1
+            counts[tuple(cell + d)] += 1
 
     next_active = set(map(lambda p: p[0], filter(lambda p: is_active(p[0] in state, p[1]), counts.items())))
     return next_active
 
-for i in range(6):
+for i in range(generations):
     active = iterate(active)
 
 print(len(active))
