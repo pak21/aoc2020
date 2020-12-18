464c734fa29620d22cd2d1382a4b812dd6292745
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-18 09:01:37 +0000

    Look, I can write a parser after all.

--- 18/18.py
+++ 18/18.py
@@ -2,37 +2,56 @@
 
 import sys
 
-def parse_line(l):
-    return list(filter(lambda c: c != ' ', l))
 
 with open(sys.argv[1]) as f:
-    expressions = list(map(lambda l: parse_line(l.strip()), f.readlines()))
+    expressions = f.readlines()
 
-def evaluate_expression(it):
-    last_op = None
-    result = 0
-    while True:
-        c = next(it, None)
-        if not c:
-            return result
-        if c == '(':
-            v = evaluate_expression(it) 
-        elif c == ')':
-            return result
-        elif c == '+':
-            last_op = '+'
-            continue
-        elif c == '*':
-            last_op = '*'
-            continue
+PART1_PRIORITIES = {'+': 0, '*': 0}
+PART2_PRIORITIES = {'+': 1, '*': 0}
+
+REDUCERS = {
+    '+': lambda a, b: a + b,
+    '*': lambda a, b: a * b
+}
+
+def reduce_operator(value_stack, operator_stack):
+    a = value_stack[-2]
+    b = value_stack[-1]
+    o = operator_stack[-1]
+    v = REDUCERS[o](a, b)
+    return value_stack[:-2] + [v], operator_stack[:-1]
+
+def evaluate_simple(tokens, priorities):
+    value_stack = []
+    operator_stack = []
+
+    for t in tokens:
+        if t == '+' or t == '*':
+            if operator_stack and priorities[t] <= priorities[operator_stack[-1]]:
+                value_stack, operator_stack = reduce_operator(value_stack, operator_stack)
+            operator_stack.append(t)
         else:
-            v = int(c)
+            value_stack.append(t)
 
-        if not last_op:
-            result = v
-        elif last_op == '+':
-            result = result + v
-        elif last_op == '*':
-            result = result * v
+    while operator_stack:
+        value_stack, operator_stack = reduce_operator(value_stack, operator_stack)
 
-print(sum(map(lambda e: evaluate_expression(iter(e)), expressions)))
+    return value_stack[0]
+
+def evaluate_expression(it, priorities):
+    tokens = []
+    while True:
+        c = next(it)
+        if c == '\n' or c == ')':
+            return evaluate_simple(tokens, priorities)
+        elif c == ' ':
+            pass
+        elif c == '(':
+            tokens.append(evaluate_expression(it, priorities))
+        elif c == '+' or c == '*':
+            tokens.append(c)
+        else:
+            tokens.append(int(c))
+
+print(sum(map(lambda e: evaluate_expression(iter(e), PART1_PRIORITIES), expressions)))
+print(sum(map(lambda e: evaluate_expression(iter(e), PART2_PRIORITIES), expressions)))
