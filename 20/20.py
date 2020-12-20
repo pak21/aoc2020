0df3f7346d05fd976f64a663269b3fc4072c9541
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-20 11:28:40 +0000

    Tidy, if not more efficient.

--- 20/20.py
+++ 20/20.py
@@ -9,7 +9,7 @@ import numpy as np
 SIZE=10
 
 def parse_line(text):
-    return list(map(lambda c: c == '#', text))
+    return list(map(lambda c: 1 if c == '#' else 0, text))
 
 def parse_tile(text):
     lines = text.split('\n')
@@ -22,10 +22,7 @@ with open(sys.argv[1]) as f:
 
 first_tile_id, first_cells = list(tiles.items())[0]
 
-tile_locations = {
-    (0, 0): first_cells
-}
-
+tile_locations = {(0, 0): first_cells}
 tile_id_locations = {(0, 0): first_tile_id}
 
 def gen_rotations(cells):
@@ -34,6 +31,21 @@ def gen_rotations(cells):
         yield np.fliplr(cells)
         cells = np.rot90(cells)
 
+def check_direction(tiles, current_tile, seen, step, old_edge_fn, new_edge_fn):
+    next_loc = (current_loc[0] + step[0], current_loc[1] + step[1])
+    edge = old_edge_fn(current_tile)
+
+    for tid, cells in tiles.items():
+        if tid in seen:
+            continue
+        for new_cells in gen_rotations(cells):
+            new_edge = new_edge_fn(new_cells)
+            if np.all(edge == new_edge):
+                tile_locations[next_loc] = new_cells
+                tile_id_locations[next_loc] = tid
+                seen.add(tid)
+                todo.append(next_loc)
+
 seen = {first_tile_id}
 todo = [(0, 0)]
 
@@ -41,65 +53,10 @@ while todo:
     current_loc = todo.pop()
     current_tile = tile_locations[current_loc]
 
-    # Right
-    next_loc = (current_loc[0], current_loc[1] + 1)
-    edge = current_tile[:,SIZE-1]
-
-    for tid, cells in tiles.items():
-        if tid in seen:
-            continue
-        for new_cells in gen_rotations(cells):
-            new_edge = new_cells[:,0]
-            if np.all(edge == new_edge):
-                tile_locations[next_loc] = new_cells
-                tile_id_locations[next_loc] = tid
-                seen.add(tid)
-                todo.append(next_loc)
-
-    # Up 
-    next_loc = (current_loc[0] - 1, current_loc[1])
-    edge = current_tile[0,:]
-
-    for tid, cells in tiles.items():
-        if tid in seen:
-            continue
-        for new_cells in gen_rotations(cells):
-            new_edge = new_cells[SIZE-1,:]
-            if np.all(edge == new_edge):
-                tile_locations[next_loc] = new_cells
-                tile_id_locations[next_loc] = tid
-                seen.add(tid)
-                todo.append(next_loc)
-
-    # Left
-    next_loc = (current_loc[0], current_loc[1] - 1)
-    edge = current_tile[:,0]
-
-    for tid, cells in tiles.items():
-        if tid in seen:
-            continue
-        for new_cells in gen_rotations(cells):
-            new_edge = new_cells[:,SIZE-1]
-            if np.all(edge == new_edge):
-                tile_locations[next_loc] = new_cells
-                tile_id_locations[next_loc] = tid
-                seen.add(tid)
-                todo.append(next_loc)
-
-    # Down
-    next_loc = (current_loc[0] + 1, current_loc[1])
-    edge = current_tile[SIZE-1,:]
-
-    for tid, cells in tiles.items():
-        if tid in seen:
-            continue
-        for new_cells in gen_rotations(cells):
-            new_edge = new_cells[0,:]
-            if np.all(edge == new_edge):
-                tile_locations[next_loc] = new_cells
-                tile_id_locations[next_loc] = tid
-                seen.add(tid)
-                todo.append(next_loc)
+    check_direction(tiles, current_tile, seen, (0, 1), lambda c: c[:,SIZE-1], lambda c: c[:,0])
+    check_direction(tiles, current_tile, seen, (-1, 0), lambda c: c[0,:], lambda c: c[SIZE-1,:])
+    check_direction(tiles, current_tile, seen, (0, -1), lambda c: c[:,0], lambda c: c[:,SIZE-1])
+    check_direction(tiles, current_tile, seen, (1, 0), lambda c: c[SIZE-1,:], lambda c: c[0,:])
 
 ys = list(map(lambda l: l[0], tile_locations.keys()))
 xs = list(map(lambda l: l[1], tile_locations.keys()))
@@ -107,22 +64,11 @@ xs = list(map(lambda l: l[1], tile_locations.keys()))
 min_y, max_y = min(ys), max(ys)
 min_x, max_x = min(xs), max(xs)
 
-top_left = (min_y, min_x)
-top_right = (min_y, max_x)
-bottom_left = (max_y, min_x)
-bottom_right = (max_y, max_x)
+print(math.prod(map(lambda l: tile_id_locations[l], [(min_y, min_x), (min_y, max_x), (max_y, min_x), (max_y, max_x)])))
+
+without_borders = dict(map(lambda p: ((p[0][0] - min_y, p[0][1] - min_x), p[1][1:-1,1:-1]), tile_locations.items()))
 
 y_range = (max_y - min_y + 1)
-
-corners = (top_left, top_right, bottom_left, bottom_right)
-corner_ids = list(map(lambda l: tile_id_locations[l], corners))
-print(math.prod(corner_ids))
-
-def remove_borders(cells):
-    return cells[1:SIZE-1,1:SIZE-1]
-
-without_borders = dict(map(lambda p: ((p[0][0] - min_y, p[0][1] - min_x), remove_borders(p[1])), tile_locations.items()))
-
 image_4d = np.zeros((y_range, y_range, SIZE-2, SIZE-2))
 for loc, small_image_2d in without_borders.items():
     image_4d[loc[0],loc[1],:,:] = small_image_2d
@@ -133,7 +79,7 @@ monster_string = """                  #
 #    ##    ##    ###
  #  #  #  #  #  #   """
 
-monster = np.array(list(map(lambda l: list(map(lambda c: 1 if c == '#' else 0, l)), monster_string.split('\n'))))
+monster = np.array(list(map(parse_line, monster_string.split('\n'))))
 monster_sum = np.sum(monster)
 
 def find_monster(image):
@@ -141,13 +87,10 @@ def find_monster(image):
     for y0 in range(image.shape[0]-monster.shape[0]+1):
         for x0 in range(image.shape[1]-monster.shape[1]+1):
             section = image[y0:y0+monster.shape[0],x0:x0+monster.shape[1]]
-            overlap = np.sum(section * monster)
-            if overlap == monster_sum:
+            overlap = section * monster
+            if np.all(overlap == monster):
                 monster_count += 1
 
-    if monster_count > 0:
-        roughness = np.sum(image) - monster_count * monster_sum
-        print(roughness)
+    return monster_count
 
-for foo in gen_rotations(image_2d):
-    find_monster(foo)
+print(list(map(lambda mc: np.sum(image_2d) - mc * np.sum(monster), (filter(None, (map(find_monster, gen_rotations(image_2d))))))))
