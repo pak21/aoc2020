7453f8bbaaba8ac328970e596a143d720788f864
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-21 06:35:26 +0000

    More tidying, still not more efficient.

--- 20/20.py
+++ 20/20.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import collections
 import math
 import sys
 
@@ -9,7 +8,12 @@ import numpy as np
 SIZE=10
 
 def parse_line(text):
-    return list(map(lambda c: 1 if c == '#' else 0, text))
+    return list(map(lambda c: c == '#', text))
+
+monster_string = """                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   """
+monster = np.array(list(map(parse_line, monster_string.split('\n'))))
 
 def parse_tile(text):
     lines = text.split('\n')
@@ -20,16 +24,15 @@ def parse_tile(text):
 with open(sys.argv[1]) as f:
     tiles = dict(map(parse_tile, f.read().strip().split('\n\n')))
 
+# Pick any tile and orientation to start the "jigaaw"
 first_tile_id, first_cells = list(tiles.items())[0]
-
-tile_locations = {(0, 0): first_cells}
-tile_id_locations = {(0, 0): first_tile_id}
+tile_locations = {(0, 0): {'id': first_tile_id, 'cells': first_cells}}
 
 def gen_rotations(cells):
     for i in range(4):
-        yield cells
-        yield np.fliplr(cells)
-        cells = np.rot90(cells)
+        rotated = np.rot90(cells, i)
+        yield rotated
+        yield np.fliplr(rotated)
 
 def check_direction(tiles, current_tile, seen, step, old_edge_fn, new_edge_fn):
     next_loc = (current_loc[0] + step[0], current_loc[1] + step[1])
@@ -41,8 +44,7 @@ def check_direction(tiles, current_tile, seen, step, old_edge_fn, new_edge_fn):
         for new_cells in gen_rotations(cells):
             new_edge = new_edge_fn(new_cells)
             if np.all(edge == new_edge):
-                tile_locations[next_loc] = new_cells
-                tile_id_locations[next_loc] = tid
+                tile_locations[next_loc] = {'id': tid, 'cells': new_cells}
                 seen.add(tid)
                 todo.append(next_loc)
 
@@ -51,7 +53,7 @@ todo = [(0, 0)]
 
 while todo:
     current_loc = todo.pop()
-    current_tile = tile_locations[current_loc]
+    current_tile = tile_locations[current_loc]['cells']
 
     check_direction(tiles, current_tile, seen, (0, 1), lambda c: c[:,SIZE-1], lambda c: c[:,0])
     check_direction(tiles, current_tile, seen, (-1, 0), lambda c: c[0,:], lambda c: c[SIZE-1,:])
@@ -64,23 +66,16 @@ xs = list(map(lambda l: l[1], tile_locations.keys()))
 min_y, max_y = min(ys), max(ys)
 min_x, max_x = min(xs), max(xs)
 
-print(math.prod(map(lambda l: tile_id_locations[l], [(min_y, min_x), (min_y, max_x), (max_y, min_x), (max_y, max_x)])))
+print(math.prod(map(lambda l: tile_locations[l]['id'], [(min_y, min_x), (min_y, max_x), (max_y, min_x), (max_y, max_x)])))
 
-without_borders = dict(map(lambda p: ((p[0][0] - min_y, p[0][1] - min_x), p[1][1:-1,1:-1]), tile_locations.items()))
+without_borders = dict(map(lambda p: ((p[0][0] - min_y, p[0][1] - min_x), p[1]['cells'][1:-1,1:-1]), tile_locations.items()))
 
 y_range = (max_y - min_y + 1)
-image_4d = np.zeros((y_range, y_range, SIZE-2, SIZE-2))
+image_4d = np.zeros((y_range, SIZE-2, y_range, SIZE-2))
 for loc, small_image_2d in without_borders.items():
-    image_4d[loc[0],loc[1],:,:] = small_image_2d
+    image_4d[loc[0],:,loc[1],:] = small_image_2d
 
-image_2d = image_4d.transpose((0,2,1,3)).reshape((y_range * (SIZE-2), y_range * (SIZE-2)))
-
-monster_string = """                  # 
-#    ##    ##    ###
- #  #  #  #  #  #   """
-
-monster = np.array(list(map(parse_line, monster_string.split('\n'))))
-monster_sum = np.sum(monster)
+image_2d = image_4d.reshape((y_range * (SIZE-2), y_range * (SIZE-2)))
 
 def find_monster(image):
     monster_count = 0
