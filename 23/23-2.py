ad5b5ee4b8d8cff315240f3f83575212d6709e12
Phil Kendall <philip@shadowmagic.org.uk>
2020-12-23 06:48:48 +0000

    Minor tidy ups.

--- 23/23-2.py
+++ 23/23-2.py
@@ -5,7 +5,6 @@ import sys
 class Cup:
     def __init__(self, cup_id):
         self.cup_id = cup_id
-        self.next_cup = None
 
 with open(sys.argv[1]) as f:
     starting_ids = list(map(int, f.readline().strip()))
@@ -13,13 +12,13 @@ with open(sys.argv[1]) as f:
 moves = int(sys.argv[2])
 max_cup = int(sys.argv[3])
 
-cups = [None] + list(map(lambda i: Cup(i+1), range(max_cup)))
+cups = list(map(Cup, range(max_cup+1)))
 
 first = cups[starting_ids[0]]
 prev = first
 for i in starting_ids[1:]:
     cup = cups[i]
-    prev.next_cup = cups[i]
+    prev.next_cup = cup
     prev = cup
 
 for i in range(len(starting_ids)+1, max_cup+1):
@@ -49,14 +48,12 @@ for m in range(moves):
             break
 
     target = cups[destination]
-
-    after = target.next_cup
+    picked_up.next_cup.next_cup.next_cup = target.next_cup
     target.next_cup = picked_up
-    picked_up.next_cup.next_cup.next_cup = after
 
     current = current.next_cup
 
 a = cups[1].next_cup
 b = a.next_cup
 
-print(a, b, a.cup_id * b.cup_id)
+print(a.cup_id, b.cup_id, a.cup_id * b.cup_id)
